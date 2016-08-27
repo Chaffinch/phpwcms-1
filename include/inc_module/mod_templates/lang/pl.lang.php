2a45307487d0862496c6c05e05934fcd026b1d00
Chaffinch <bobi@poczta.fm>
2016-08-27 07:42:26 +0200

    Create pl.lang.php
    Polish language

---
+++ include/inc_module/mod_templates/lang/pl.lang.php
@@ -0,0 +1,22 @@
+<?php
+// Language: Polish, Language Code: pl UTF-8 for ver.1.8.4 (2016/07/27)
+//Zięba Bogusław http://www.krynica.malopolska.pl
+$BLM['backend_menu']	= 'Menedżer Szablonów';
+$BLM['listing_title']	= 'Menedżer Szablonów';
+
+$BLM['file_copy']	= 'Kopiuj szablon';
+$BLM['file_rename']	= 'Zmień nazwę szablonu';
+$BLM['file_delete']	= 'Usuń szablon';
+$BLM['list_files']	= 'Artykuł z tej listy szablonów';
+$BLM['show_code']	= 'Pokaż kod źródłowy szablonu';
+$BLM['label_default']	= 'Szablon Domyślny (/inc_default)';
+$BLM['label_folder']	= 'Folder docelowy';
+$BLM['label_custom']	= 'Szablony';
+$BLM['label_sample']	= 'Przykładowy Folder';
+$BLM['success_msg']	= ' Szablon skopiowany z powodzeniem. Proszę zmienić nazwę i dostosować szablon.';
+$BLM['success_msg_error']	= ' Szablon nie może być skopiowany.';
+$BLM['deleted_msg']		= ' Szablon usunięty z powodzeniem.';
+$BLM['deleted_msg_error']	= ' Szablon nie może być usunięty.';
+$BLM['rename_msg']		= ' Z powodzeniem zmieniono nazwę szablonu.';
+$BLM['rename_msg_error']	= ' Nazwa szablonu nie może zostać zmieniona.';
+?>
